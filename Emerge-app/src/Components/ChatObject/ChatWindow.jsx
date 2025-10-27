99302f5457cbbb694760f8468b6117dd1271dd03
javamande <magnus.m.andersen@hotmail.com>
2025-10-27 10:31:16 +0900

    "Components clean up "

--- Emerge-app/src/Components/ChatObject/ChatWindow.jsx
+++ Emerge-app/src/Components/ChatObject/ChatWindow.jsx
@@ -1,4 +1,6 @@
 // Components/Chat/ChatWindow.jsx
+
+//Not currently in use - for later
 export default function ChatWindow({ header, children }) {
     return (
       <section className="chat-pane">
