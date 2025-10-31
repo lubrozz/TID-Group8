5fb389c4afde2b9e93f627fdafc99237d74c6780
Lukas <luoe@itu.dk>
2025-10-31 10:29:57 +0100

    Created NewChildChat page. Along with components needed for the page.

---
+++ Emerge-app/src/Components/ChildChat/ExitButton.jsx
@@ -0,0 +1,9 @@
+import "../../chat.css";
+
+export default function ExitButton() {
+  return (
+    <button className="exitButton">
+      <p>Exit</p>
+    </button>
+  );
+}
--- Emerge-app/src/Components/Shared/NewChatButton.jsx
+++ Emerge-app/src/Components/Shared/NewChatButton.jsx
@@ -2,7 +2,7 @@ import { Link } from "react-router-dom";
 
 export default function NewChatButton() {
   return (
-    <Link to="/ChildChat" style={{ textDecoration: "none" }}>
+    <Link to="/NewChildChat" style={{ textDecoration: "none" }}>
       <div className="new-chat-button">
         <h4 className="arrow-label-text">New chat</h4>
         <span className="arrow-label-icon">&#10140;</span>
