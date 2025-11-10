f379f3f51cc722e15117f7216beb51cc4cdfebc1
luoe <luoe@itu.dk>
2025-11-10 15:32:02 +0100

    Correct initialization from the back4app guide.

--- Emerge-app/src/App.jsx
+++ Emerge-app/src/App.jsx
@@ -1,22 +1,24 @@
-import Parse from "parse/dist/parse.min.js";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
+import { useEffect } from "react";
+import Parse from "parse/dist/parse.min.js";
 import HomePage from "./Pages/HomePage";
 import ProfChat from "./Pages/ProfChat";
 import NewChildChat from "./Pages/NewChildChat";
 
-export default function App() {
+function initializeParse() {
   Parse.initialize(
     "zMmQlW49bZ8poXbFzKOokRbWvlwJf2Jd9BYqjvTI",
     "LmonXzQwyZ5p5qp1TxiI3tVrRcg2PZ89ug9I5iai"
   );
+  Parse.serverURL = "https://parseapi.back4app.com";
+}
 
-  Parse.serverURL = "https://parseapi.back4app.com"; //migration to happen soon.
+export default function App() {
+  useEffect(() => {
+    initializeParse();
+  }, []);
   return (
     <>
-      <script
-        type="text/javascript"
-        src="https://unpkg.com/parse/dist/parse.min.js"
-      ></script>
       <Router>
         <Routes>
           <Route path="/" element={<HomePage />} />
