cd4d9ff72e5bf4ce618e05cd84d615f94500cbe8
javamande <magnus.m.andersen@hotmail.com>
2025-11-10 16:16:06 +0100

    "LoginRerouting"

--- Emerge-app/src/App.jsx
+++ Emerge-app/src/App.jsx
@@ -2,7 +2,9 @@ import Parse from "parse/dist/parse.min.js";
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./Pages/HomePage";
 import ProfChat from "./Pages/ProfChat";
+import LoginPage from "./Pages/LoginPage";
 import NewChildChat from "./Pages/NewChildChat";
+
 
 export default function App() {
   Parse.initialize(
@@ -16,6 +18,7 @@ export default function App() {
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/prof-chat" element={<ProfChat />} />
+        <Route path="/prof-login" element={<LoginPage />} />
         <Route path="/new-child-chat" element={<NewChildChat />} />
         <Route path="*" element={<Navigate to="/" />} />
       </Routes>
