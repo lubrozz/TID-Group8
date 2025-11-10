2552d2e9822e583d9774882a38519dd7a692d9f4
luoe <luoe@itu.dk>
2025-11-10 14:22:57 +0100

    Merged incoming/outgoing message bubbles into MessageBubble. Still needs more work.

---
+++ Emerge-app/src/Components/ChildChat/MessageBubble.jsx
@@ -0,0 +1,17 @@
+import "../../chat.css";
+
+export default function MessageBubble({ bubbleStyle, isProf }) {
+  return (
+    <div className={bubbleStyle}>
+      {isProf && <img src=".avatar.png" alt="" />}
+      <div className="texts">
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
+          magni nihil eligendi, mollitia repellat dolor sunt voluptas iste minus
+          dolore itaque, illo ut possimus alias rem ipsam ex odio voluptatibus.
+        </p>
+        <span>1 min ago</span>
+      </div>
+    </div>
+  );
+}
