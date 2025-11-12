605d120a3eb90aac0b90ec135660eba38bf926d3
luoe <luoe@itu.dk>
2025-11-12 12:46:07 +0100

    Corrected back-end setup

--- Emerge-app/src/App.jsx
+++ Emerge-app/src/App.jsx
@@ -1,6 +1,4 @@
 import { HashRouter as Router, Routes, Route } from "react-router-dom";
-import { useEffect } from "react";
-import Parse from "parse/dist/parse.min.js";
 import HomePage from "./Pages/HomePage";
 import ProfChat from "./Pages/ProfChat";
 import NewChildChat from "./Pages/NewChildChat";
