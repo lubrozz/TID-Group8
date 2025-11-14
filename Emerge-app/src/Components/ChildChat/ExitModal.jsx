96e8741daac20fb96191015dc8779c0cbc4bb9da
Lukas Østergaard <lukas.ostergaard@abacusmedicine.com>
2025-11-14 14:05:14 +0100

    New styling for exit chat modal

--- Emerge-app/src/Components/ChildChat/ExitModal.jsx
+++ Emerge-app/src/Components/ChildChat/ExitModal.jsx
@@ -1,9 +1,4 @@
-import { useState } from "react";
-import Dialog from "@mui/material/Dialog";
-import DialogTitle from "@mui/material/DialogTitle";
-import DialogContent from "@mui/material/DialogContent";
-import DialogContentText from "@mui/material/DialogContentText";
-import DialogActions from "@mui/material/DialogActions";
+import { useEffect, useRef, useState } from "react";
 import { Link } from "react-router-dom";
 import "../../child-chat.css";
 
@@ -11,6 +6,16 @@ export default function ExitModal() {
   const [open, setOpen] = useState(false);
   const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
+
+  const dialogRef = useRef();
+
+  useEffect(() => {
+    if (open) {
+      dialogRef.current.showModal(); // used to create modal and backdrop
+    } else {
+      dialogRef.current.close();
+    }
+  }, [open]);
 
   return (
     <div>
@@ -22,31 +27,23 @@ export default function ExitModal() {
           Exit
         </button>
       </div>
-      <div className="popup">
-        <Dialog open={open} onClose={handleClose}>
-          <DialogTitle id="alert-dialog-title">
-            {"Exit and delete this chat?"}
-          </DialogTitle>
-          <DialogContent>
-            <DialogContentText id="alert-dialog-description">
-              If you exit the chat, your conversation will be deleted. But you
-              are always welcome to come back to have another chat.
-            </DialogContentText>
-          </DialogContent>
-          <div>
-            <DialogActions className="actionButtons">
-              <button className="cancelExitButton" onClick={handleClose}>
+      <dialog className="dialog" ref={dialogRef} onClose={handleClose}>
+          <h2>Exit and delete this chat?</h2>
+          <p>If you exit the chat, your conversation will be deleted. But you
+              are always welcome to come back to have another chat.</p>
+          <div className="actionButtons">
+            <button className="cancelExitButton" onClick={handleClose}>
+              <p className="texts">
                 Back to chat
+              </p>
               </button>
               <button className="confirmExitButton">
                 <Link to="/" className="texts">
                   Exit Chat
                 </Link>
               </button>
-            </DialogActions>
           </div>
-        </Dialog>
-      </div>
+      </dialog>
     </div>
   );
 }
