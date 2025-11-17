21819cd2dffab16e50d6a74e5b8a95f4a6c2ecf2
luoe <luoe@itu.dk>
2025-11-17 15:40:01 +0100

    Deleted unused buttons

--- Emerge-app/src/Components/ToBeDeleted/LoginButton.jsx
+++
@@ -1,9 +0,0 @@
-import "../../styles/LoginPage.css";
-
-export default function LoginButton({ onClick }) {
-  return (
-    <button className="login-button" onClick={onClick}>
-      Log in
-    </button>
-  );
-}
--- Emerge-app/src/Components/ToBeDeleted/RegisterButton.jsx
+++
@@ -1,9 +0,0 @@
-import "../../styles/LoginPage.css";
-
-export default function RegisterButton({ onClick }) {
-  return (
-    <button className="Register-button" onClick={onClick}>
-      Register
-    </button>
-  );
-}
