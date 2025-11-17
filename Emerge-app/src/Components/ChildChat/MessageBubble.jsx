1e42421de6d3e2277c647785a452aea711b78861
Lukas <luoe@itu.dk>
2025-11-17 23:00:16 +0100

    setup sending messages dynamically and saving them to database

--- Emerge-app/src/Components/ChildChat/MessageBubble.jsx
+++ Emerge-app/src/Components/ChildChat/MessageBubble.jsx
@@ -1,17 +1,10 @@
 import "../../styles/child-chat.css";
 
-export default function MessageBubble({ bubbleStyle, isProf }) {
+export default function MessageBubble({ bubbleStyle, isProf, children }) {
   return (
     <div className={bubbleStyle}>
       {isProf && <img src=".avatar.png" alt="" />}
-      <div className="texts">
-        <p>
-          Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati
-          magni nihil eligendi, mollitia repellat dolor sunt voluptas iste minus
-          dolore itaque, illo ut possimus alias rem ipsam ex odio voluptatibus.
-        </p>
-        <span>1 min ago</span>
-      </div>
+      <div className="texts">{children}</div>
     </div>
   );
 }
