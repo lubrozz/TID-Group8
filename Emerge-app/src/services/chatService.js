6850c1bb5e0e1c4383529831b64ce6dcf750cc2b
Lukas Østergaard <lukas.ostergaard@abacusmedicine.com>
2025-11-20 15:27:48 +0100

    changed backend calls to cloud function

--- Emerge-app/src/services/chatService.js
+++ Emerge-app/src/services/chatService.js
@@ -1,21 +1,7 @@
 import Parse from "parse"; // <-- Important
-import { findRandomProfUser, createNewAnonUser } from "./userService";
 
 export async function createNewChatRoom() {
-  const deleteAfter = new Date(Date.now());
-  deleteAfter.setUTCDate(deleteAfter.getUTCDate() + 30);
-
-  const pro = await findRandomProfUser();
-  const anon = await createNewAnonUser();
-
-  const newChatRoom = new Parse.Object("ChatRoom");
-  newChatRoom.set("pro", pro);
-  newChatRoom.set("anon", anon);
-  newChatRoom.set("status", "open");
-  newChatRoom.set("deleteAfter", deleteAfter);
-  newChatRoom.set("anonDisplayName", "Anonymous user");
-
-  return await newChatRoom.save();
+  return Parse.Cloud.run("createNewChatRoom");
 }
 
 export async function sendMessage(text, chatRoomId) {
